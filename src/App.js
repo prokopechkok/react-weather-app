48f67445d4b37ee5274609bafc5a70fc11ce563e
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-03-06 16:21:42 +0200

    added github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project created by Khrystyna Prokopechko and is{" "}
+        <a
+          href="https://github.com/prokopechkok/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
