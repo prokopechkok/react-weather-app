091af01c9679e8d2a055a3cf08b1f818e0a0a51b
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-03-07 09:56:51 +0200

    moved HTML and CSS from vanila project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project created by Khrystyna Prokopechko and is{" "}
           <a
---
+++ src/Weather.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+          <div className="row">
+            <div className="col-6">
+              <div className="clearfix weather-temperature">
+                <img src="" alt="cloudy" id="icon" className="float-left" />
+                <span className="float-left">
+                  <strong id="temperature"></strong>
+                  <span className="units">
+                    <a href="/" id="celsius-link" className="active">
+                      °C
+                    </a>{" "}
+                    |
+                    <a href="/" id="fahrenheit-link">
+                      °F
+                    </a>
+                  </span>
+                </span>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  Humidity: <span id="humidity"></span>%
+                </li>
+                <li>
+                  Wind: <span id="wind"></span> m/h
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <div className="overview">
+                <h1 id="city"></h1>
+                <ul>
+                  <li>
+                    <span id="date"></span>
+                  </li>
+                  <li id="description"></li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-6">
+              <form id="search-form" className="">
+                <input
+                  type="search"
+                  placeholder="Type a city..."
+                  className="form-control"
+                  id="city-input"
+                  autoComplete="off"
+                />
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary w-30"
+                />
+              </form>
+            </div>
+          </div>
+          <div className="weather-forecast" id="forecast"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
