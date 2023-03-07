1777d3202b869f3d294ae1c02c0b50508039146e
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-03-07 09:12:57 +0200

    addea bootstrap dependancy, upgraded npm

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project created by Khrystyna Prokopechko and is{" "}
-        <a
-          href="https://github.com/prokopechkok/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project created by Khrystyna Prokopechko and is{" "}
+          <a
+            href="https://github.com/prokopechkok/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
